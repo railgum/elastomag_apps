e73803ae211b6983a60adbf25c53c596a642c732
Rail <railbolo@gmail.com>
2024-12-13 21:20:13 +0400

    коммит

--- src/app_for_tk/main.py
+++ src/app_for_tk/main.py
@@ -1,7 +1,7 @@
 from window import Window
 
 
-window = Window(700, 300, "Создание сводной таблицы",
+window = Window(700, 300, "Создание справки и сводной таблицы",
                 icon=None)
 window.label.configure(
     text="@Автор программы - Раиль Гумеров.\nПриветствую вас в моем первом .exe-шном приложении!",
