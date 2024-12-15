26adca9bc6e1aa37a589f4fa44b451ffe1ab5f8a
Rail <railbolo@gmail.com>
2024-12-15 21:00:19 +0400

    изменил класс главного окна

--- src/app_for_tk/main.py
+++ src/app_for_tk/main.py
@@ -1,16 +1,13 @@
-from window import Window
+from windows import Main_Window
 
 
-window = Window(700, 300, "Создание справки и сводной таблицы",
-                icon=None)
+window = Main_Window(700, 300, "Создание справки и сводной таблицы",
+                     icon=None)
 window.label.configure(
     text="@Автор программы - Раиль Гумеров.\nПриветствую вас в моем первом .exe-шном приложении!",
     fg="green"
 )
-window.button.configure(text="Выберите папку", command=window.select_folder)
-window.place_entry.set("Путь до папки")
-
-window.entry.configure(width=50, fg="blue")
+window.entry.configure(width=50, fg="grey")
 
 if __name__ == "__main__":
     window.run()
--- src/app_for_tk/windows.py
+++ src/app_for_tk/windows.py
@@ -2,40 +2,44 @@ from tkinter import *
 from tkinter import filedialog as fd
 
 
-class Window:
+FONT_MAIN_WINDOW = ("Times New Roman", 18)
+
+
+class Main_Window:
     def __init__(self, width, height, title="MyWindow", resizable=(False, False), icon=None):
         self.root = Tk()
+        self.label = None
+        self.entry = None
+        self.place_entry = None
         self.root.title(title)
         self.root.geometry(f"{width}x{height}+200+200")
         self.root.resizable(resizable[0], resizable[1])
         if icon:
             self.root.iconbitmap(icon)
+
+        self.draw_widgets()
+
+    def draw_widgets(self):
         self.label = Label(
             self.root,
             text="Label",
-            font=("Times New Roman", 18)
+            font=FONT_MAIN_WINDOW
         )
-        self.button = Button(self.root, text="Button",
-                             command=self.select_folder)
+        button = Button(self.root, text="Button",
+                        command=self.select_folder)
+        button.configure(text="Выберите папку", command=self.select_folder)
         self.place_entry = StringVar(value="Text")
+        self.place_entry.set("Путь до папки")
         self.entry = Entry(self.root, width=30, textvariable=self.place_entry)
-
-    def run(self):
-        self.draw_widgets()
-        self.root.mainloop()
-
-    def draw_widgets(self):
         self.label.grid(row=0, column=0, columnspan=2,
                         padx=20, pady=20, sticky="NSEW")
         self.entry.grid(row=1, column=0, padx=20, pady=20)
-        self.button.grid(row=1, column=1, padx=20, pady=20)
+        button.grid(row=1, column=1, padx=20, pady=20)
 
     def select_folder(self):
         folder = fd.askdirectory()
         self.place_entry.set(folder)
         return folder
 
-
-# if __name__ == "__main__":
-#     window = Window(500, 500)
-#     window.run()
+    def run(self):
+        self.root.mainloop()
